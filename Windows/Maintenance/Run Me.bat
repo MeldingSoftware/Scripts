afad1379d82d6024f8027f8c22fe7b077b43c9bf
MeldingSoftware <155779315+MeldingSoftware@users.noreply.github.com>
2026-08-26 18:04:20 -0400

    Delete Windows/Maintenance directory

--- Windows/Maintenance/Run Me.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "SCRIPT=%~dp0Maintenance.ps1"
-
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "Start-Process PowerShell -Verb RunAs -WorkingDirectory '%~dp0' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%SCRIPT%""'"
